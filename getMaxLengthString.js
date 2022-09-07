725ad24c11b14caf30fe1d87fe9c759d71f677c9
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2022-09-07 16:03:39 +0700

    2 failed, 3 passed

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,20 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if (arrayOfString === undefined || arrayOfString === null){
+    return undefined
+  }
+  let arr = arrayOfString
+  let newarr = []
+  let x
+  for (let i = 0; i < arr.length ; i++) {
+    for (let j = i+1; j < arr.length ; j++) {
+      if (arr[i].length >= arr[j].length){
+        x = arr[i]
+      }
+    }
+  }
+  newarr.push(x)
+  return newarr
 }
 module.exports = getMaxLengthString
